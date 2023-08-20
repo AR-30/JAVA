c4e3548f38845e1c5bf4e00368050ec888bd77b8
AR-30 <112623706+AR-30@users.noreply.github.com>
2023-08-20 20:32:00 +0530

    Create problem1Excel.java

---
+++ problem1Excel.java
@@ -0,0 +1,29 @@
+//Given an excel sheet row or column value, you have to find out the value in decimal. For example, A -> 1, AB -> 28, ADD -> 784.
+
+import java.util.Scanner;
+import java.lang.Math;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+        String s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("Enter the string");
+	    Scanner sc = new Scanner(System.in);
+	    String input = sc.next();
+	    String str = input.toUpperCase();
+	    System.out.println("Entered string "+str);
+	    double res=0;
+	    int pow=0;
+	    for(int i=str.length()-1; i>=0; i--){
+	        for(int j=0; j<s.length(); j++){
+	            if(str.charAt(i)==s.charAt(j)){
+	                res=res+Math.pow(26,pow)*(j+1);
+	                pow++;
+	            }
+	        }
+	    }
+	    System.out.println(res);
+	    
+	}
+}

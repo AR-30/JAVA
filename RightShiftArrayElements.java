dec539d4d887644709d8fd265fcc3280a4a684b9
AR-30 <112623706+AR-30@users.noreply.github.com>
2023-08-31 22:49:54 +0530

    Update RightShiftArrayElements.java

--- RightShiftArrayElements.java
+++ RightShiftArrayElements.java
@@ -42,3 +42,46 @@ public class Main
 		}
 	}
 }
+
+
+
+//BRUTE FORCE METHOD
+
+
+import java.util.Scanner;
+public class Main
+{
+    public static void rightShift(int arr[], int k, int size){
+       for (int i=0; i<k; i++) {
+            int last = arr[size-1];
+            for (int j = size-1; j>0; j--) {
+                arr[j] = arr[j-1];
+            }
+            arr[0] = last;
+        }
+    }
+    
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input Array size");
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		System.out.println("Input Array");
+		for(int i=0; i<size; i++){
+		    arr[i]= sc.nextInt();
+		}
+		System.out.println("Enter k value");
+		int k = sc.nextInt();
+		
+		if(k>size){
+		    k=k%size;
+		}
+		
+		rightShift(arr,k,size);
+		
+		System.out.println("Resultant array:");
+		for(int i=0; i<size; i++){
+		    System.out.print(arr[i]+" ");
+		}
+	}
+}

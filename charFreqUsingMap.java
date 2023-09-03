4b25c6697187b696eb3349e20dc8302bcd95a866
AR-30 <112623706+AR-30@users.noreply.github.com>
2023-09-03 17:30:19 +0530

    Update charFreqUsingMap.java

--- charFreqUsingMap.java
+++ charFreqUsingMap.java
@@ -12,18 +12,18 @@ public class Main
 		String s = sc.nextLine();
 		
 		Map<Character, Integer> map = new HashMap<Character, Integer>();
-        for(int i=0; i<s.length(); i++){
-            char c=s.charAt(i);
-            if(map.containsKey(c)){
-                int freq = map.get(c);
-                map.put(c, freq+1);
-            }
-            else{
-                map.put(c, 1);
-            }
-        }
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + ":" + entry.getValue());
-        }
+        	for(int i=0; i<s.length(); i++){
+            		char c=s.charAt(i);
+            		if(map.containsKey(c)){
+                		int freq = map.get(c);
+                		map.put(c, freq+1);
+            		}
+            		else{
+               	 		map.put(c, 1);
+            		}
+        	}
+        	for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            		System.out.println(entry.getKey() + ":" + entry.getValue());
+        	}
 	}
 }

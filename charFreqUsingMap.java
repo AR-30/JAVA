4633fbc8a9a5ef29e86ba3fb5c28594fdc434598
AR-30 <112623706+AR-30@users.noreply.github.com>
2023-09-03 17:07:20 +0530

    Create charFreqUsingMap.java

---
+++ charFreqUsingMap.java
@@ -0,0 +1,29 @@
+//Find frequency of characters using map.
+
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter String :");
+		String s = sc.nextLine();
+		
+		Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for(int i=0; i<s.length(); i++){
+            char c=s.charAt(i);
+            if(map.containsKey(c)){
+                int freq = map.get(c);
+                map.put(c, freq+1);
+            }
+            else{
+                map.put(c, 1);
+            }
+        }
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+	}
+}

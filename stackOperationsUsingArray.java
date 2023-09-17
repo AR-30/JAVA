97b34644375a87dd2a7d0fc5b9386c164aba7f22
AR-30 <112623706+AR-30@users.noreply.github.com>
2023-09-17 23:33:38 +0530

    Create stackOperationsUsingArray.java

---
+++ stackOperationsUsingArray.java
@@ -0,0 +1,45 @@
+//stack using array
+
+import java.util.Scanner;
+
+public class Main { 
+    public static void main(String[] args) { 
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter size of stack "); 
+        int size=sc.nextInt(); 
+        int[] stack = new int[size]; 
+        int top=-1; 
+        while(true){ 
+            System.out.println("Choose operation :\n1:push()\n2:pop()\n3:display()\n4:exit"); 
+            int choice =sc.nextInt(); 
+            switch(choice) { 
+                case 1: 
+                    if(top==size-1) 
+                        System.out.println("Stack Overflow"); 
+                    else{ 
+                         System.out.print("Enter element:"); 
+                         int ele = sc.nextInt(); 
+                         stack[++top]=ele; 
+                    } 
+                    break; 
+                case 2: 
+                    if(top==-1) 
+                        System.out.println("Stack Underflow"); 
+                    else 
+                        System.out.println("Popped element = "+top--); 
+                    break; 
+                case 3: 
+                    System.out.print("The stack contents are "); 
+                    for(int i=top; i>-1; i--){ 
+                        System.out.print(stack[i] + " "); 
+                    }
+                    System.out.println();
+                    break; 
+            } 
+            if(choice==4){
+                System.out.println("Program terminated!!");
+                return;
+            }
+        } 
+    } 
+}

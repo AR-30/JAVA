2fe6a1b7e380720e6e7f94de1056a63c77181982
APARNA RAJPUT <112623706+AR-30@users.noreply.github.com>
2023-10-16 22:52:52 +0530

    Update queueUsingArray.java

--- queueUsingArray.java
+++ queueUsingArray.java
@@ -1,82 +1,81 @@
-import java.util.*;
-public class Queue {
-	  int SIZE = 5;
-	  int items[] = new int[SIZE];
-	  int front, rear;
-	  Queue(){
-	    front = 0;
-	    rear = -1;
-	  }	  
-	  boolean isFull(){
-	    if (front == 0 && rear == SIZE - 1) 
+import java.util.Scanner;
+class Queue {
+	static int front, rear, capacity;
+	static int queue[];
+	Queue(int c){
+		front = rear = -1;
+		capacity = c;
+		queue = new int[capacity];
+	}
+	static boolean isFull(){
+	    if (rear == capacity - 1) 
 	      return true;
 	    return false;
-	  }
-	  boolean isEmpty() {
-	    if (front == -1)
-	      return true;
-	    else
-	      return false;
 	 }
-	 void enqueue(int element) {
- 		if (isFull())
-	      	System.out.println("Queue is full");
-	    else{
-	        if (front == -1)
-	             front = 0;
-	        rear++;
-	        items[rear] = element;
-	        System.out.println("Insert " + element);
-	    	}
-	  }
-	  int dequeue(){
-	    int element;
-	    if (isEmpty()){
+	static boolean isEmpty() {
+	    if (front == -1 || front>rear )
+	      return true;
+	    return false;
+	}
+	static void enqueue(int data){
+		if (isFull()) {
+			System.out.printf("\nCapacity exhausted, could not insert "+data+"\n");
+		}
+		else {
+		    if(front==-1)
+		        front++;
+		    rear++;
+			queue[rear] = data;
+		}
+		return;
+	}
+	static void dequeue(){
+		int element;
+	    if (isEmpty())
 	      System.out.println("Queue is empty");
-	      return (-1);
-	    }
 	    else{
-	      element = items[front++];
+	      element = queue[front++];
 	      System.out.println( element + " Deleted");
-	      return (element);
 	    }
-	  }
-	   void display() {
+	}
+	void display() {
 	    int i;
 	    if (isEmpty()) 
 	      System.out.println("Empty Queue");
 	    else {
-	      System.out.println("\nFront index-> " + front);
-	      System.out.println("Items -> ");
-	      for (i = front; i <= rear; i++)
-	        System.out.print(items[i] + "  ");
-                System.out.println("\nRear index-> " + rear);
+	        System.out.println("\nFront index-> " + front);
+	        System.out.println("Queue -> ");
+	        for (i = front; i <= rear; i++)
+	            System.out.print(queue[i] + "  ");
+            System.out.println("\nRear index-> " + rear);
 	    }
 	  }
-	 public static void main(String[] args) {
-     	Queue Q = new Queue();
+	  public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter size of queue "); 
+        int size=sc.nextInt();
+     	Queue Q = new Queue(size);
 	    System.out.println("Initial Stack Empty : " + Q.isEmpty());
      	while(true){
-            System.out.println("Enter 1: enqueue 2: dequeue 3:dsplay 4: exit");
-	  		Scanner sc= new Scanner(System.in); 
-	  		System.out.println("Enter the option ");
+            System.out.println("Enter your choice\n1: enqueue\n2: dequeue\n3: display\n4: exit");
 	  		int ch= sc.nextInt();
-		    	switch(ch)
-		    	{
-		    	case 1: System.out.println("Enter the value to be inserted into Q");
-		    			Scanner sc1= new Scanner(System.in); 
-		    			System.out.println("Enter first element ");
-		    			int val= sc1.nextInt();
-		    			Q.enqueue(val);
-		    			break;
-		         case 2: Q.dequeue();
-		              	 Q.display();
-		               break;
-		         case 3: Q.display();
-		         break;
-		    	case 4: 
-		    	return;
+		    	switch(ch){
+		    	case 1:
+		    	    System.out.println("Enter the value to be inserted");
+		    		int val= sc.nextInt();
+		    		Q.enqueue(val);
+		   			break;
+		         case 2:
+		             Q.dequeue();
+		             break;
+		         case 3:
+		            Q.display();
+		            break;
+		    	case 4:
+		    	    return;
+		    	default:
+		    	    System.out.println("Enter valid choice:");
 		    	}
 		    }
-		}	    
-	}
+		}
+}

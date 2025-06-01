bbfe9b87497f36b9fb8d88d2e884e9d65edecc65
APARNA RAJPUT <112623706+AR-30@users.noreply.github.com>
2025-06-01 19:25:28 +0530

    Update sorting_arr_of_0,1,2_without_algo.java

--- sorting_arr_of_0,1,2_without_algo.java
+++ sorting_arr_of_0,1,2_without_algo.java
@@ -37,9 +37,44 @@ public class Main
 	        i++;
 	        z--;
 	    }
-	    System.out.print("Sorted array : ")
+	    System.out.print("Sorted array : ");
 	    for(int j=0; j<n; j++){
 	        System.out.print(arr[j]+" ");
 	    }
 	}
+}
+
+
+
+//ANOTHER WAY - >
+
+// with static input
+
+class Main {
+    public static void main(String[] args) {
+        int[] arr={0,0,2,1,2};
+        int n=arr.length;
+        int a=0,b=0,c=0;
+        for(int i=0; i<n;i++){
+            if(arr[i]==0)
+                a++;
+            else if(arr[i]==1)
+                b++;
+            else
+                c++;
+        }
+        b=b+a;
+        c=c+b;
+        for(int i=0; i<n; i++){
+            if(i<a)
+                arr[i]=0;
+            else if(i<b)
+                arr[i]=1;
+            else
+                arr[i]=2;
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 }

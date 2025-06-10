7578512748138b74eab24d1b2c5d305adc2e2ade
APARNA <112623706+AR-30@users.noreply.github.com>
2025-06-11 00:24:01 +0530

    Create reverseString.java

---
+++ reverseString.java
@@ -0,0 +1,14 @@
+//Reverse a String
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+       String str = "qwerty";
+        String reversedStr = "";
+        for (int i=str.length()-1; i>=0; i--) {
+            reversedStr+=str.charAt(i);
+        }
+        System.out.print(reversedStr); 
+    }
+}

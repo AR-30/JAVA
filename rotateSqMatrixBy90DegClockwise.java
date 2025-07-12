a3e57f644be6d4b360510ffb95b51d59de8fc065
APARNA <112623706+AR-30@users.noreply.github.com>
2025-07-13 02:04:25 +0530

    Update rotateSqMatrixBy90DegClockwise.java

--- rotateSqMatrixBy90DegClockwise.java
+++ rotateSqMatrixBy90DegClockwise.java
@@ -1,1 +1,40 @@
+//Rotate matrix by 90 degrees clockwise
+//Time - O(n^2) and Space - O(1) 
 
+class Main {
+    public static void main(String[] args) {
+        int[][] mat = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+        
+        int n = mat[0].length;
+        
+        //transposing matrix
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+        //reversing rows
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n/2; j++){
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][n-j-1];
+                mat[i][n-j-1] = temp;
+            }
+        }
+        
+        //printing results
+        for (int[] row : mat) {
+            for (int x : row) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}

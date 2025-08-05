1044c199dcf0b4630d1e617ec56f48c2026d218c
APARNA <112623706+AR-30@users.noreply.github.com>
2025-08-05 10:23:52 +0530

    Create reverseLLusingPointers.java

---
+++ reverseLLusingPointers.java
@@ -0,0 +1,63 @@
+//Write a Program to reverse the Linked List. (Both Iterative and recursive)
+//Iterative
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        LinkedList l = new LinkedList();
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+            String x = sc.next();
+            l.insertL(x);
+        }
+        l.reverse();
+        l.display();
+    }
+}
+
+class Node{
+    String data;
+    Node next;
+    Node(String data){
+        this.data = data;
+        next = null;
+    }
+}
+class LinkedList{
+    Node head = null;
+    
+    void insertL(String data){
+        Node n = new Node(data);
+        if(head == null){
+            head = n;
+        }else{
+            Node temp = head;
+            while(temp.next!=null){
+                temp = temp.next;
+            }
+            temp.next = n;
+        }
+    }
+    void reverse(){
+        Node t1 = null;
+        Node t2 = head;
+        Node t3 = null;
+        while(t2!=null){
+            t3 = t2.next;
+            t2.next = t1;
+            t1 = t2;
+            t2 = t3;
+        }
+        head = t1;
+    }
+    void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
